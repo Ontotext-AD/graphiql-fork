096e0edda8b2bcacdee1016b4ca5ac3b6a60996b
boyantonchev <boyan.tonchev@ontotext.com>
2025-02-14 10:44:18 +0200

    GDB-11239: Extend GraphiQLProviderProps with newly added translation properties
    ## What
     Extend the GraphiQLProviderProps with newly added translation properties.
    
    ## Why
    By providing translation to GraphiQL, the properties that can be passed to it have increased to include "selectedLanguage" and "translations."
    
    ## How
    Create a type definition for the translation provider's properties and add it to the GraphiQL provider's properties.

---
+++ packages/graphiql-react/src/translation/models/translation-context-provider-props.ts
@@ -0,0 +1,32 @@
+import {Translations} from './translations';
+
+export type TranslationContextProviderProps = {
+  /**
+   * Determined the language that has to be used.
+   */
+  selectedLanguage?: string
+  
+  /**
+   * Represents a collection of translations for multiple languages,
+   * using a flat structure with dot-separated keys or nested objects.
+   *
+   * @example
+   * ```typescript
+   * const translations: Translations = {
+   *   en: {
+   *     "editor.title": "Editor",
+   *     "editor": {
+   *       "description": "This is the editor"
+   *     }
+   *   },
+   *   fr: {
+   *     "editor.title": "Éditeur",
+   *     "editor": {
+   *       "description": "C'est l'éditeur"
+   *     }
+   *   }
+   * };
+   * ```
+   */
+  translations?: Translations;
+}
